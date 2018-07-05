5d2d65e975873faf7a1942df354bb6e0ddd3a4b7
Agyesam <Agyesam2@hotmail.com>
2018-07-05 07:42:09 +0000

    Adding DbContext and Configfuration

--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,8 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options =>
+            options.UseSqlServer("...."))
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
